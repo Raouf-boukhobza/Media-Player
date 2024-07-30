9a45448b0ef16e13759572e7727e295ebcd9f30d
raouf <raoufboukhobza3@gmail.com>
2024-07-30 15:22:19 +0100

    fix the circularslider

--- app/src/main/java/com/raouf/audioplayer/ui/MainActivity.kt
+++ app/src/main/java/com/raouf/audioplayer/ui/MainActivity.kt
@@ -7,33 +7,35 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.pager.PageSize.Fill.calculateMainAxisPageSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
+import com.raouf.audioplayer.R
 import com.raouf.audioplayer.ui.theme.AudioPlayerTheme
 import com.raouf.audioplayer.ui.theme.purpule
 import dagger.hilt.android.AndroidEntryPoint
-import kotlin.math.PI
-import kotlin.math.atan2
-import kotlin.math.cos
-import kotlin.math.sin
 
 
 @AndroidEntryPoint
@@ -48,64 +50,60 @@ class MainActivity : ComponentActivity() {
                   arrayOf(Manifest.permission.READ_MEDIA_AUDIO),
                   0
               )
-
-
-
-
+                Scaffold(modifier = Modifier.fillMaxSize()){paddingValues ->  
+                       Column(modifier = Modifier
+                           .fillMaxSize()
+                           .padding(paddingValues),
+                           verticalArrangement = Arrangement.Center,
+                           horizontalAlignment = Alignment.CenterHorizontally){
+                         MusicSlider()
+                       }
+                }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun HalfCircleSlider() {
-    var angle by remember { mutableStateOf(0f) }
-    val radius = 150f
-    Canvas(
-        modifier = Modifier
-            .size(200.dp)
-            .padding(16.dp)
-            .pointerInput(Unit) {
-                detectDragGestures { change, _ ->
-                    val x = change.position.x - radius
-                    val y = change.position.y - radius
-                    val theta = atan2(y, x)
-                    val newAngle = (theta * (180 / Math.PI) + 180).toFloat()
-                    if (newAngle in 0f..190f){
-                        angle = newAngle
-                    }
-                }
-            }
-    ) {
-        // Draw the half circle arc
-        drawArc(
-            color = Color.LightGray,
-            startAngle = 180f,
-            sweepAngle = 180f,
-            useCenter = false,
-            size = Size(radius * 2, radius * 2),
-            topLeft = Offset(0f, 0f),
-            style = Stroke(width = 14f)
-        )
-        drawArc(
-            color = purpule,
-            startAngle = 180f,
-            sweepAngle = angle,
-            useCenter = false,
-            size = Size(radius * 2, radius * 2),
-            topLeft = Offset(0f, 0f),
-            style = Stroke(width = 14f),
-        )
-        // Calculate the thumb position
-        val thumbX = radius + radius * cos((angle - 180) * (PI / 180)).toFloat()
-        val thumbY = radius + radius * sin((angle - 180) * (PI / 180)).toFloat()
 
-        // Draw the thumb
-        drawCircle(
-            color = purpule,
-            radius = 16f,
-            center = Offset(thumbX, thumbY)
+@Preview(showBackground = true , showSystemUi = true)
+@Composable
+fun MusicSlider(){
+    Box(contentAlignment = Alignment.Center , modifier = Modifier.fillMaxSize()){
+        CircularSlider(
+            modifier = Modifier.size(275.dp),
+            stroke = 25f,
+            progressColor = purpule,
+            thumbColor = purpule,
+            onChange = {}
         )
+        Image(
+            painter = painterResource(id = R.drawable.mellow),
+            contentDescription = null ,
+            modifier = Modifier
+                .clip(shape = CircleShape)
+                .size(200.dp),
+            contentScale = ContentScale.Crop
+
+        )
+
+
+        Box(modifier = Modifier.height(275.dp) ,
+            contentAlignment = Alignment.BottomCenter){
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .background(color = purpule)
+                    .size(55.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.PlayArrow,
+                    contentDescription =  null ,
+                    modifier = Modifier.size(35.dp),
+                    tint = Color.White
+                )
+            }
+        }
     }
 }
+
